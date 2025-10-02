7deb13b684ba96bd99a41af59e1e912d34fb8e75
JustinBelangerAC <bela0485@algonquinlive.com>
2025-10-02 18:57:07 -0400

    add install for ubuntu 24

---
+++ db/install-mongoDB-Ubuntu24.04.sh
@@ -0,0 +1,19 @@
+sudo apt update
+
+# install deps
+sudo apt-get install gnupg curl
+
+# import the public key
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
+   --dearmor
+
+# create the list file
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.2.list
+sudo apt-get update
+
+# install mongoDB
+sudo apt-get install -y mongodb-org
+
+#run mongo DB
+sudo systemctl start mongod
