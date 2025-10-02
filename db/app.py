cd44ce399df382f7bb673b2b86e5b44cd018aec8
JustinBelangerAC <bela0485@algonquinlive.com>
2025-10-02 19:47:06 -0400

    Populate using json file. Fetch from IDs

--- db/app.py
+++ db/app.py
@@ -1,4 +1,5 @@
 import pymongo
+import json
 
 # start the mongo client
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -9,41 +10,52 @@ mydb = myclient["Crocs"]
 # add a new collection called crocodiles
 crocodiles = mydb["crocodiles"]
 
-# create a croco
-croco1 = {
-            "Observation ID":1,
-            "Common Name":"Morelet's Crocodile",
-            "Scientific Name":"Crocodylus moreletii",
-            "Family":"Crocodylidae",
-            "Genus":"Crocodylus",
-            "Observed Length (m)":1.9,
-            "Observed Weight (kg)":62.0,
-            "Age Class":"Adult",
-            "Sex":"Male",
-            "Date of Observation":"31-03-2018",
-            "Country\/Region":"Belize",
-            "Habitat Type":"Swamps",
-            "Conservation Status":"Least Concern",
-            "Observer Name":"Allison Hill",
-            "Notes":"Cause bill scientist nation opportunity."
-        }
+# create an empty query, it will return every item
+getAllQuery = {}
 
-# add the croco to the collection
-x = crocodiles.insert_one(croco1)
+# clear the collection, we do this so that we can re-run the script without duping the data
+crocodiles.delete_many(getAllQuery)
+
+# populate the DB with sample data
+try:
+    # try to open the json file with the list of sample data
+    with open('crocodile_dataset.json', 'r') as f:
+        data = json.load(f)
+
+    crocodiles.insert_many(data)
+# if the json file is missing
+except FileNotFoundError:
+    print("Error: 'data.json' not found.")
+
+# if the json file is in a bad format
+except json.JSONDecodeError:
+    print("Error: Could not decode JSON from 'crocodile_dataset.json'. Check file format.")
 
 # list the DBs
-print("List of databases")
+print("List of databases in the mongoDB client")
 print(myclient.list_database_names())
 
 # list the collections
-print("List of collections in the DB")
+print("List of collections in the Crocs DB")
 print(mydb.list_collection_names())
 
-#address greater than S:
-myquery = {}
 
-croclist = crocodiles.find(myquery)
+# Get all Crocos in the DB
+def get_all_crocos():
+    print("Got a request to list all crocos")
+    croc_result = crocodiles.find(getAllQuery)
+    return croc_result
 
-# list crocs
-for x in croclist:
-    print(x)
+
+# Get a Croco based on an ID
+def get_croco(ID):
+    print("Got a request get croco " + str(ID))
+    croco_query = {"_id" : ID}
+    croc_result = crocodiles.find_one(croco_query)
+    return croc_result
+
+# Create a Croco
+
+# Edit a Croco based on their ID
+
+# Delete a Croco based on their ID
