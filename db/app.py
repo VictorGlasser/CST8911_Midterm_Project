8c97da681d52b108a90816585ceb7ebf7bc6cc4f
JustinBelangerAC <bela0485@algonquinlive.com>
2025-10-02 21:05:03 -0400

    Added console output

--- db/app.py
+++ db/app.py
@@ -17,45 +17,24 @@ getAllQuery = {}
 crocodiles.delete_many(getAllQuery)
 
 # populate the DB with sample data
+print("Database created!")
+print("Populating database with JSON file data.....")
 try:
+    print("Attempting to read 'crocodile_dataset.json'.....")
     # try to open the json file with the list of sample data
     with open('crocodile_dataset.json', 'r') as f:
         data = json.load(f)
+        print("Reading complete!")
 
+    print("Loading JSON data into the database.....")
     crocodiles.insert_many(data)
+
+    print("Loading of JSON data is complete!")
+    print("Welcome to the special crocodile database!")
 # if the json file is missing
 except FileNotFoundError:
-    print("Error: 'data.json' not found.")
+    print("Error: 'crocodile_dataset.json' not found.")
 
 # if the json file is in a bad format
 except json.JSONDecodeError:
     print("Error: Could not decode JSON from 'crocodile_dataset.json'. Check file format.")
-
-# list the DBs
-print("List of databases in the mongoDB client")
-print(myclient.list_database_names())
-
-# list the collections
-print("List of collections in the Crocs DB")
-print(mydb.list_collection_names())
-
-
-# Get all Crocos in the DB
-def get_all_crocos():
-    print("Got a request to list all crocos")
-    croc_result = crocodiles.find(getAllQuery)
-    return croc_result
-
-
-# Get a Croco based on an ID
-def get_croco(ID):
-    print("Got a request get croco " + str(ID))
-    croco_query = {"_id" : ID}
-    croc_result = crocodiles.find_one(croco_query)
-    return croc_result
-
-# Create a Croco
-
-# Edit a Croco based on their ID
-
-# Delete a Croco based on their ID
